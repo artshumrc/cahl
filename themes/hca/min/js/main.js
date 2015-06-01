afcd74ffee87495c5cb937406d01a1463aee7bb4
0restes <lukehollis@gmail.com>
2015-06-01 10:39:08 -0400

    add french language support

--- themes/hca/min/js/main.js
+++ themes/hca/min/js/main.js
@@ -7,14 +7,37 @@ angular.module('HarvardCharlieApp')
 	window.__Hc__ = window.__Hc__ || {};
 	var Hc = window.__Hc__;
 
+	$scope.language = "english";
 	$scope.successful_submission = false;
-
 	$scope.submission = {
 
 	};
 
+	$scope.start = function(){
+
+		if (document.cookie.indexOf("harvard_charlie_language") >= 0) {
+			$scope.language = $.cookie("harvard_charlie_language");
+			$(".selected-language").removeClass("selected-language");
+			$(".language[data-language='" + $scope.language + "']").addClass("selected-language");
+
+		}else{
+			$.cookie("harvard_charlie_language", "english");
+			$scope.language = "english";
+
+		}
+
+		$scope.update();
+	};
+
 	$scope.update = function(){
-	}
+
+		if ($scope.language=="francais"){
+			document.title = "Archives Charlie à Harvard";
+		}else{
+			document.title = "The Charlie Archive at the Harvard Library | Department of Romance Languages and Literatures and Harvard Libraries";
+		}
+
+	};
 
 	$scope.submit = function(){
 
@@ -25,8 +48,21 @@ angular.module('HarvardCharlieApp')
 
 	};
 
-	$scope.update();
+	$scope.toggle_language = function(e){
 
+		var $target = $(e.target)
+		,	new_language = $target.data().language
+		;
+
+		$scope.language = new_language;
+		$(".selected-language").removeClass("selected-language");
+		$(".language[data-language='" + $scope.language + "']").addClass("selected-language");
+		$.cookie("harvard_charlie_language", new_language);
+
+		$scope.update();
+	};
+
+	$scope.start();
 
 
 
