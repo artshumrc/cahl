d455ea22ca5b047f5171079c2e0bdee92faf5c0f
0restes <lukehollis@gmail.com>
2015-07-02 00:33:35 +0200

    updates for the language change in labels

--- themes/hca/min/js/main.js
+++ themes/hca/min/js/main.js
@@ -25,7 +25,6 @@ angular.module('HarvardCharlieApp')
 			$scope.language = "english";
 
 		}
-
 		$scope.update();
 	};
 
@@ -37,6 +36,7 @@ angular.module('HarvardCharlieApp')
 			document.title = "The Charlie Archive at the Harvard Library | Department of Romance Languages and Literatures and Harvard Libraries";
 		}
 
+		$scope.update_form_labels();
 	};
 
 	$scope.submit = function(){
@@ -91,99 +91,47 @@ angular.module('HarvardCharlieApp')
 		$(".language[data-language='" + $scope.language + "']").addClass("selected-language");
 		$.cookie("harvard_charlie_language", new_language);
 
+
+
 		$scope.update();
 	};
 
-	$scope.upload_files = function(e){
-
-		var fd = new FormData()
-
-		for (var i in $scope.files) {
-
-			formData.append("action", "upload-attachment");
-			fd.append("uploadedFile", $scope.files[i])
-			  var fileInputElement = document.getElementById("file");
-			formData.append("async-upload", fileInputElement.files[0]);
-			formData.append("name", fileInputElement.files[0].name);
-				
-		}
-
-		var xhr = new XMLHttpRequest()
-
-		xhr.upload.addEventListener("progress", uploadProgress, false)
-		xhr.addEventListener("load", uploadComplete, false)
-		xhr.addEventListener("error", uploadFailed, false)
-		xhr.addEventListener("abort", uploadCanceled, false)
-		xhr.open("POST", "/fileupload")
-
-		$scope.progressVisible = true
-		xhr.send(fd)
+	$scope.update_form_labels = function( ){
 
 
-		function uploadProgress(e) {
-			$scope.$apply(function(){
+		$(".wpcf7-list-item-label").each(function( i, label ){
 
-				if (e.lengthComputable) {
-					$scope.progress = Math.round(e.loaded * 100 / e.total);
+			if ( $scope.language === "english" ){
 
-				} else {
-					$scope.progress = 'unable to compute';
+				if ( label.textContent === "Oui" ){
+					label.textContent = "Yes";
+
+				}else if ( label.textContent === "Non" ){
+					label.textContent = "No";
 
 				}
 
-			});
-		}
-
-		function uploadComplete(e) {
-			/* This event is raised when the server send back a response */
-			alert(e.target.responseText)
-		}
-
-		function uploadFailed(e) {
-			alert("There was an error attempting to upload the file.")
-		}
-
-		function uploadCanceled(e) {
-			$scope.$apply(function(){
-				$scope.progressVisible = false;
-			})
-			alert("The upload has been canceled by the user or the browser dropped the connection.")
-		}
-
-	};
+			}else {
 
 
+				if ( label.textContent === "Yes" ){
+					label.textContent = "Oui";
 
-	$scope.setFiles = function(element) {
-
-		var allowed_filetypes = ["png", "jpg", "jpeg", "tiff", "tif"];
-
-		$scope.$apply(function(scope) {
-
-			// Turn the FileList object into an Array
-			console.log('files:', element.files);
-			scope.files = []
-
-			for (var i = 0; i < element.files.length; i++) {
-
-				for (var j = 0; j < allowed_filetypes.length; j++){
-					var fname = element.files[i].name
-					,	file_extension = allowed_filetypes[j]
-					;
-
-					if( fname.indexOf( file_extension, fname.length - file_extension.length ) !== -1 ){
-						scope.files.push(element.files[i])
-						break;
-					}
+				}else if ( label.textContent === "No" ){
+					label.textContent = "Non";
 
 				}
 
 			}
 
-			scope.progressVisible = false
 
 		});
+
+
+
+
 	};
+
 
 
 
