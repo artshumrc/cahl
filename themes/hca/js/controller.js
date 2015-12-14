f9f14e9f71cfb637d8a1697d0120b76edb0bee75
Luke Hollis <lukehollis@gmail.com>
2015-12-13 23:37:17 -0500

    updates for single search tools

--- themes/hca/js/controller.js
+++ themes/hca/js/controller.js
@@ -11,36 +11,48 @@ angular.module('cahl')
 
 	};
 	$scope.search_results = [{
+				id : 1,
+				tags : ["Photograph", "Je suis Charlie"],
 				thumbnail : "/wp-content/themes/hca/min/img/submission5.jpg",
 				title : "Example Submission 1",
 				author : "Example Submitter",
 				date : "Feb 23, 2015",
 				location : "Paris, FR"
 			},{
+				id : 2,
+				tags : ["Graffiti", "Photograph"],
 				thumbnail : "/wp-content/themes/hca/min/img/submission2.jpg",
 				title : "Example Submission 2",
 				author : "Example Submitter",
 				date : "Jul 7, 2015",
 				location : "Boston, MA, USA"
 			},{
+				id : 3,
+				tags : ["News & Articles", "Magazine"],
 				thumbnail : "/wp-content/themes/hca/min/img/submission4.jpg",
 				title : "Example Submission 3",
 				author : "Example Submitter",
 				date : "Jan 30, 2015",
 				location : "Berlin, DE"
 			},{
+				id : 4,
+				tags : ["Photograph"],
 				thumbnail : "/wp-content/themes/hca/min/img/event_soldiers.jpg",
 				title : "Example Submission 4",
 				author : "Example Submitter",
 				date : "Feb 2, 2015",
 				location : "Paris, FR"
 			},{
+				id : 5,
+				tags : ["News & Articles"],
 				thumbnail : "/wp-content/themes/hca/min/img/submission3.jpg",
 				title : "Example Submission 5",
 				author : "Example Submitter",
 				date : "Mar 19, 2015",
 				location : "Boston, MA, USA"
 			},{
+				id : 6,
+				tags : ["Photograph", "Graffiti"],
 				thumbnail : "/wp-content/themes/hca/min/img/submission6.jpg",
 				title : "Example Submission 6",
 				author : "Example Submitter",
@@ -48,7 +60,7 @@ angular.module('cahl')
 				location : "Paris, FR"
 		}];
 
-	$scope.start = function(){
+	$scope.init = function(){
 
 		if (document.cookie.indexOf("harvard_charlie_language") >= 0) {
 			$scope.language = $.cookie("harvard_charlie_language");
@@ -174,10 +186,48 @@ angular.module('cahl')
 	};
 
 
+	$scope.show_single = function ( e ){
+		var $target = $(e.target)
+		,		target_id
+		;
+		if( !$target.hasClass("submission-teaser") ){
+			$target = $target.parents(".submission-teaser");
+
+		}
+
+		/*
+		setTimeout(function(){
+			$("#selected_result_image").elevateZoom({
+				zoomType	: "lens",
+				lensShape : "round",
+				lensSize  : 200
+			});
+
+		}, 1000);
+		*/
+
+		target_id = $target.data().id;
+
+		$scope.search_results.forEach(function(result){
+
+			if ( result.id === target_id ){
+				$scope.selected_result = result;
+			}
+
+		});
 
 
-	$scope.start();
+	};
+
+	$scope.close_single = function ( e ){
+		$scope.selected_result = null;
+
+	};
 
 
 
-	}]);
+	$scope.init();
+
+
+
+}]);
